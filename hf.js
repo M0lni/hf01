058e4bedc8e7a82cebe2590b55d24c57158cdcff
Your Name <you@example.com>
2021-10-21 18:37:40 +0200

    v1.0

--- hf.js
+++ hf.js
@@ -6,7 +6,7 @@
   // csak tesztelgetem a githubot
 
   
-  const readline = require('readline');
+ /*  const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -16,4 +16,28 @@ const rl = readline.createInterface({
 rl.question('What do you think of Node.js? ', (answer) => {
   console.log(`Thank you for your valuable feedback: ${answer}`);
   rl.close();
-});
+}); */
+
+function koszones(emberek){
+    if(emberek=="")
+    return "Hello, my friend";
+}
+
+
+
+function nevek() {
+    let emberek = prompt();
+    let text;
+    text=koszones(emberek);
+
+    document.getElementById("demo").innerHTML = text;
+    
+  }
+ 
+  
+ /*  if (person == null || person == "") {
+    text = "Hello, my friend";
+  } else {
+    text = "Hello " + person + "! How are you today?";
+  }
+  document.getElementById("demo").innerHTML = text; */
--- hf01.test.js
+++ hf01.test.js
@@ -1,8 +1,10 @@
-const koszones = require('./hf');
+/*const sum = require('./hf');
 
-/*test('adds 1 + 2 to equal 3', () => {
+test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });*/
+
+const koszones=require('./hf');
 
 test('Tests for no input', () => {
     expect(koszones("")).toBe("Hello, my friend");
