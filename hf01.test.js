f3969cc50dd37264834434ddea9a8bf46645e23f
Your Name <you@example.com>
2021-10-21 21:58:04 +0200

    v5.0

--- hf01.test.js
+++ hf01.test.js
@@ -7,16 +7,19 @@ test('adds 1 + 2 to equal 3', () => {
 const koszones=require('./hf');
 
 test('Tests for no input', () => {
-    expect(koszones("")).toBe("Hello, my friend");
+    expect(koszones("")).toBe("Hello, my friend.");
   });
 
 test('Tests for 1 input', () => {
-    expect(koszones("Fred")).toBe("Hello, Fred");
+    expect(koszones("Fred")).toBe("Hello, Fred.");
   });
 
 test('Tests for shouting', () => {
     expect(koszones("BOB")).toBe("HELLO, BOB!");
   });
 test('Tests for multiple names', () => {
-    expect(koszones("Sam, Dave, Bob")).toBe("Hello, Sam, Dave and Bob");
+    expect(koszones("Sam, Dave, Bob")).toBe("Hello, Sam, Dave and Bob.");
+  });
+  test('Tests for multiple names that can be uppercase', () => {
+    expect(koszones("Sam, DAVE, Bob, FERENC")).toBe("Hello, Sam and Bob. AND HELLO DAVE AND FERENC");
   });
